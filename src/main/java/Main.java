4231e14039a037c65f5ea05745aaff55d86e7670
KKip <ttlapik@gmail.com>
2020-02-17 15:22:14 -0500

    changed from disc4j to javacord

--- src/main/java/Main.java
+++
@@ -1,7 +0,0 @@
-import java.io.PrintStream;
-import static sun.misc.Version.print;
-
-public class Main {
-    public static void main(string[] args) {
-    }
-}
---
+++ src/main/java/discBot/main.java
@@ -0,0 +1,9 @@
+package discBot;
+
+public class main {
+    public static void main(String[] args){
+        System.out.println("hey man");
+    }
+}
+
+// note to deal with imports + consequences
