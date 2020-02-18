017210c0cc053ceea6e5a9e108e89a40468099f5
InsidiousData <55824797+InsidiousData@users.noreply.github.com>
2020-02-17 16:00:14 -0800

    Javacord feature test
    Edited dependencies to make work.
    Needs cleanup in depedencies.

--- src/main/java/discBot/main.java
+++ src/main/java/discBot/main.java
@@ -1,8 +1,22 @@
 package discBot;
 
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+
 public class main {
-    public static void main(String[] args){
-        System.out.println("hey man");
+    public static void main(String[] args) {
+        // Insert bot's token here
+        String token = "Njc4MTEyMTQxMTU2Mjg2NDk0.XkskLQ.S2kt8DW2ZP0IjwtR6jRka1PXUzE";
+
+        DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
+
+        // Replies with "Pong!" if someone writes "!ping"
+        api.addMessageCreateListener(event -> {
+            if (event.getMessage().getContent().equalsIgnoreCase("!ping")) {
+                event.getChannel().sendMessage("Pong!");
+            }
+        });
+
     }
 }
 
