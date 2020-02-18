49ad43cf72cb4cb8a82a41858fdb510ad493429e
InsidiousData <55824797+InsidiousData@users.noreply.github.com>
2020-02-18 15:47:15 -0800

    Added invite url print

--- src/main/java/discBot/main.java
+++ src/main/java/discBot/main.java
@@ -6,16 +6,19 @@ import org.javacord.api.DiscordApiBuilder;
 public class main {
     public static void main(String[] args) {
         // Insert bot's token here
-        String token = "Njc4MTEyMTQxMTU2Mjg2NDk0.XkskLQ.S2kt8DW2ZP0IjwtR6jRka1PXUzE";
+        String token = "Njc4MTEyMTQxMTU2Mjg2NDk0.XktYNg.cD7outNHQmuCjng07_XIyS2ufrM";
 
         DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
-
+  
         // Replies with "Pong!" if someone writes "!ping"
         api.addMessageCreateListener(event -> {
             if (event.getMessage().getContent().equalsIgnoreCase("!ping")) {
                 event.getChannel().sendMessage("Pong!");
             }
         });
+
+        // Print invite url
+        System.out.println("Invite url: " + api.createBotInvite());
 
     }
 }
